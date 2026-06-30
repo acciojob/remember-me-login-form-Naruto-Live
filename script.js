195ca5939ca7718577f30db2b5bf1d5f9631c590
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-07-01 00:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existing = document.getElementById("existing");
+
+if (localStorage.getItem("username")) {
+    existing.style.display = "block";
+}
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    alert(`Logged in as ${username.value}`);
+
+    if (checkbox.checked) {
+
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+
+        existing.style.display = "block";
+
+    } else {
+
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+
+        existing.style.display = "none";
+
+    }
+
+});
+
+existing.addEventListener("click", function () {
+
+    alert(`Logged in as ${localStorage.getItem("username")}`);
+
+});
